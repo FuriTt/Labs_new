54f74df8ac698ee1587b80a1ec70ae9e7901d2df
BillyYank <taras4834@gmail.com>
2020-08-29 17:20:23 +0500

    lagrange

---
+++ cpp_code/cmake-build-debug/CMakeFiles/neuton-rafson.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neuton-rafson.dir/neuton-rafson.cpp.obj"
+  "libneuton-rafson.dll.a"
+  "neuton-rafson.exe"
+  "neuton-rafson.exe.manifest"
+  "neuton-rafson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neuton-rafson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
